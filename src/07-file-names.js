a9af71d522c4debfc05a00649c491c33bea3093e
Anton Dovgun <toshi4ka7@mail.ru>
2021-04-07 09:43:30 +0300

    ready task 07

--- src/07-file-names.js
+++ src/07-file-names.js
@@ -13,8 +13,34 @@
  * the output should be ["file", "file(1)", "image", "file(1)(1)", "file(2)"]
  *
  */
-function renameFiles(/* names */) {
-  throw new Error('Not implemented');
+function renameFiles(names) {
+  if (names.length === 0) return [];
+  const res = [names[0]];
+
+  for (let i = 1; i < names.length; i++) {
+    if (res.includes(names[i])) {
+      const reg = new RegExp(`${names[i]}\\((\\d+)\\)`);
+      let add = false;
+
+      for (let j = res.length - 1; j >= 0; j--) {
+        const found = res[j].match(reg);
+        if (found) {
+          const count = Number(found[1]) + 1;
+          res.push(`${names[i]}(${count})`);
+          add = true;
+          break;
+        }
+      }
+
+      if (!add) {
+        res.push(`${names[i]}(1)`);
+      }
+    } else {
+      res.push(names[i]);
+    }
+  }
+
+  return res;
 }
 
 module.exports = renameFiles;
