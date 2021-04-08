66407e69c850dd5c4249ec586c65feb9ecbb518b
toshi4ka7 <toshi4ka7@mail.ru>
2021-04-08 10:27:43 +0300

    ready task 10

--- src/10-find-index.js
+++ src/10-find-index.js
@@ -11,8 +11,25 @@
  * For ([1, 2, 3], 2) should return 1
  *
  */
-function findIndex(/* array, value */) {
-  throw new Error('Not implemented');
+function findIndex(array, value) {
+  if (array[0] === value) return 0;
+  if (array[array.length - 1] === value) return array.length - 1;
+
+  let res;
+
+  function find(index) {
+    if (array[index] === value) {
+      res = index;
+    } else if (array[index] > value) {
+      find(Math.ceil(index / 2));
+    } else if (array[index] < value) {
+      find(Math.ceil(index + index / 2));
+    }
+  }
+
+  find(Math.ceil((array.length - 1) / 2));
+
+  return res;
 }
 
 module.exports = findIndex;
