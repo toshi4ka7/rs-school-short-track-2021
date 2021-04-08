9f87e8a4eb72ea3dc1bb66add5c21e4d20388065
toshi4ka7 <toshi4ka7@mail.ru>
2021-04-08 11:40:31 +0300

    ready task 11

--- src/11-encode-line.js
+++ src/11-encode-line.js
@@ -8,8 +8,28 @@
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new Error('Not implemented');
+function encodeLine(str) {
+  if (!str) return '';
+  let res = '';
+
+  let symbol = str[0];
+  let count = 1;
+
+  for (let i = 1; i < str.length; i++) {
+    if (str[i] === symbol) {
+      count++;
+    } else {
+      res += (count === 1 ? '' : count) + symbol;
+      symbol = str[i];
+      count = 1;
+    }
+
+    if (i === str.length - 1) {
+      res += (count === 1 ? '' : count) + symbol;
+    }
+  }
+
+  return res;
 }
 
 module.exports = encodeLine;
